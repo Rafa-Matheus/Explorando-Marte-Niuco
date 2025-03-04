a8d4ab412e1dddeefdf154aee1b1389dadbf6055
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2025-03-04 16:02:55 -0300

    Atualizado Program.cs para acionar checagem de segurança e exibir saídas para o usuário

--- Explorando-Marte-Niuco/Program.cs
+++ Explorando-Marte-Niuco/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System.Text.RegularExpressions;
+using Explorando_Marte_Niuco.Commands;
+using Explorando_Marte_Niuco.Factory;
 using Explorando_Marte_Niuco.Models;
+using Explorando_Marte_Niuco.Services;
 
 System.Console.WriteLine("--| Explorando Marte - 2025 |--");
 
@@ -12,14 +15,15 @@ var partesPlanalto = inputPlanalto!.Split(' ');
 int xMax = int.Parse(partesPlanalto[0]);
 int yMax = int.Parse(partesPlanalto[1]);
 var planalto = new Planalto(xMax, yMax);
-
+var sondaFactory = new SondaFactory(planalto);
+var sondaService = new SondaService(planalto);
 
 while(true)
 {
     System.Console.WriteLine("Digite a posição inicial da Sonda (Formato: 1 2 N/E/S/W):");
     string? posicaoInicialSonda = Console.ReadLine();
     if (!Regex.IsMatch(posicaoInicialSonda ?? "", @"^\d+\s+\d+\s+[NSEW]"))
-        throw new ArgumentException("Formato inválido. Tente novamente mais tarde.");
+        throw new ArgumentException("Formato inválido. Tente novamente mais tarde.\n");
 
     var partesPosicaoInicialSonda = posicaoInicialSonda!.Split(' ');
     int x = int.Parse(partesPosicaoInicialSonda[0]);
@@ -29,12 +33,28 @@ while(true)
     System.Console.WriteLine("Digite o trajeto do plano de operação da sonda (Formato: MLRMMRMRMLMMR):");
     string? sequenciaComandos = Console.ReadLine()?.ToUpper();
     if (string.IsNullOrEmpty(sequenciaComandos) || !Regex.IsMatch(sequenciaComandos ?? "", @"^[LRM]+$"))
-        throw new ArgumentException("Comandos com formato inválido. Use apenas L, R ou M. Tente novamente mais tarde.");
-
-    
+        throw new ArgumentException("Comandos com formato inválido. Use apenas L, R ou M. Tente novamente mais tarde.\n");
 
 
-    System.Console.WriteLine("Iniciar operação com nova sonda? Digite Y para Sim ou qualquer outra tecla para Não:");
+    try
+    {
+        System.Console.WriteLine("Checagem de Segurança. Analisando as informações da rota a ser percorrida...\n");
+        sondaService.ChecarSegurancaDoPercursoProgramado(x, y, direcao, sequenciaComandos); // Para aqui se não puder prosseguir com a operação
+        
+        System.Console.WriteLine("Operação permitida. Iniciando...\n");
+
+        var sonda = sondaFactory.CriarSonda(x, y, direcao);
+        sondaService.ExecutarComandos(sonda, sequenciaComandos);
+
+        System.Console.WriteLine($"Posição final da sonda: {sonda.X} x {sonda.Y}");
+    }
+    catch(Exception ex)
+    {
+        System.Console.WriteLine($"Erro: {ex.Message}");
+    }
+
+
+    System.Console.WriteLine("Iniciar nova operação? Digite Y para Sim ou qualquer outra tecla para Não:");
     var resposta = Console.ReadLine()?.ToUpper();
     if (resposta != "Y") break; 
 }
